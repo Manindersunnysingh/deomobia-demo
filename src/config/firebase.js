bbc3b49cd2665f5cedf7376002c7ff9559477d5b
iamsunnysingh <test@gmail.com>
2024-02-11 11:21:40 +1100

    addedDotEnv

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBoKyTqnKU1aHQuikm-gML1Y4LOjGn_xF8",
-  authDomain: "demobia-dev.firebaseapp.com",
-  projectId: "demobia-dev",
-  storageBucket: "demobia-dev.appspot.com",
-  messagingSenderId: "693565746739",
-  appId: "1:693565746739:web:1a9cd34474d7495d553d46",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
