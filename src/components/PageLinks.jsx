2cdfe2bdffe7a3598a89da4829585ca39047879d
iamsunnysingh <test@gmail.com>
2024-02-11 17:58:46 +1100

    fix broken links

--- src/components/PageLinks.jsx
+++ src/components/PageLinks.jsx
@@ -11,12 +11,12 @@ const PageLinks = ({ parentClass, itemClass }) => {
         const { text, path } = link;
         return (
           <li key={text}>
-            <a href={path} className={itemClass}>
+            {/* <a href={path} className={itemClass}>
               {text}
-            </a>
-            {/* <NavLink to={path} key={text} className={itemClass}>
+            </a> */}
+            <NavLink to={path} key={text} className={itemClass}>
               {text}
-            </NavLink> */}
+            </NavLink>
           </li>
         );
       })}
